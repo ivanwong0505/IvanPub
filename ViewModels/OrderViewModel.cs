1fc5dbb2f7601ff3e429cfb98ceb501bd593c764
ivanwong0505 <51070742+ivanwong0505@users.noreply.github.com>
2020-02-16 15:45:43 +0800

    Add files via upload

---
+++ ViewModels/OrderViewModel.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+using IvanCafe.Models;
+
+namespace IvanCafe.ViewModels
+{
+    public class OrderViewModel
+    {
+        public List<CartItem> CartItems { get; set; }
+        public decimal CartTotal { get; set; }
+    }
+}
---
+++ ViewModels/RemoveFromOrderViewModel.cs
@@ -0,0 +1,11 @@
+﻿namespace IvanCafe.ViewModels
+{
+    public class RemoveFromOrderViewModel
+    {
+        public string Message { get; set; }
+        public decimal CartTotal { get; set; }
+        public int CartCount { get; set; }
+        public int ItemCount { get; set; }
+        public int DeleteId { get; set; }
+    }
+}
