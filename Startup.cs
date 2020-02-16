8e2288b7299b6178d6ca563507ef5f86c05eebc8
ivanwong0505 <51070742+ivanwong0505@users.noreply.github.com>
2020-02-16 15:34:51 +0800

    Add files via upload

---
+++ Startup.cs
@@ -0,0 +1,15 @@
+﻿using IvanCafe;
+using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartup(typeof(Startup))]
+namespace IvanCafe
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
